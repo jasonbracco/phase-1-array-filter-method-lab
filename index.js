1ebb6153faffe8f6d7e212be83c862731f80d9fd
Jason Bracco <bracco.jason@gmail.com>
2022-05-06 13:26:04 -0400

    commit tree b31aee15ee2caa2ede22993b314a2a59a0826a79

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(function(driver){
+        return driver.toUpperCase() === string.toUpperCase() //equating the case of the string in the array to case of the arguement value
+    })
+}
+
+function fuzzyMatch(array, string){ //Need to compare the characters of a provided string
+    return array.filter(function(driver){ 
+        return driver.toLowerCase().slice(0, string.length) === string.toLowerCase() //slicing the inputs of driver to the length of the given string, and making it case unspecific, allows me to compare only the beginning letters directly
+    })
+}
+
+function matchName(array, string){
+    return array.filter(function(driver){
+            return driver.name === string
+    })
+}
